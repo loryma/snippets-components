b68c4cca0c172e2a017dc12555818a64012f6078
loryma <lorymalloc@gmail.com>
2020-04-03 19:10:16 +0300

    add dropdown

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
 import React from "react";
 import Slideshow, { Slide } from "./components/Slideshow.1";
 import Accordeon from "./components/Accordion.3";
+import Dropdown from "./components/Dropdown.4";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Slideshow>
-        <Slide caption={"Caption 1"} />
-        <Slide caption={"Caption 2"} />
-        <Slide caption={"Caption 3"} />
-        <Slide caption={"Caption 4"} />
-      </Slideshow>
+      <div className="card">
+        <Slideshow>
+          <Slide caption={"Caption 1"} />
+          <Slide caption={"Caption 2"} />
+          <Slide caption={"Caption 3"} />
+          <Slide caption={"Caption 4"} />
+        </Slideshow>
+      </div>
+      <div className="card">
+        <Accordeon title="title 1">
+          <div className="accordeon__content">content of accordeon 1</div>
+        </Accordeon>
+        <Accordeon title="title 2">
+          <div className="accordeon__content">content of accordeon 2</div>
+        </Accordeon>
+        <Accordeon title="title 3">
+          <div className="accordeon__content">content of accordeon 3</div>
+        </Accordeon>
+      </div>
+      <div className="card">
+        <Dropdown title="Hover" open="onMouseOver" close="onMouseLeave">
+          <a className="link">Link 1</a>
+          <a className="link">Link 2</a>
+          <a className="link">Link 2</a>
+        </Dropdown>
 
-      <Accordeon title="title 1">
-        <div className="accordeon__content">content of accordeon 1</div>
-      </Accordeon>
-      <Accordeon title="title 2">
-        <div className="accordeon__content">content of accordeon 2</div>
-      </Accordeon>
-      <Accordeon title="title 3">
-        <div className="accordeon__content">content of accordeon 3</div>
-      </Accordeon>
+        <Dropdown
+          title="Click"
+          open="onClick"
+          btnStyle={{ backgroundColor: "#3498DB" }}
+        >
+          <a className="link">Link 1</a>
+          <a className="link">Link 2</a>
+          <a className="link">Link 2</a>
+        </Dropdown>
+      </div>
     </div>
   );
 }
---
+++ src/components/Dropdown.4/Dropdown.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect, useRef } from "react";
+import "./Dropdown.scss";
+
+function Dropdown({
+  children,
+  title,
+  open,
+  close = "noClose",
+  onClickOutside,
+  containerStyle = {},
+  btnStyle = {}
+}) {
+  const [show, setShow] = useState(false);
+  const dropdownRef = useRef();
+
+  useEffect(() => {
+    window.addEventListener("click", handleClickOutside);
+
+    function handleClickOutside(e) {
+      const target = e.target;
+      if (!dropdownRef.current.contains(target)) {
+        setShow(false);
+      }
+    }
+
+    return () => {
+      window.removeEventListener("click", handleClickOutside);
+    };
+  }, [onClickOutside]);
+
+  return (
+    <div
+      className="dropdown"
+      ref={dropdownRef}
+      style={containerStyle}
+      {...{ [open]: () => setShow(true), [close]: () => setShow(false) }}
+    >
+      <div className="dropdown__btn" style={btnStyle}>
+        {title}
+      </div>
+      {show && <div className="dropdown__content">{children}</div>}
+    </div>
+  );
+}
+
+export default Dropdown;
